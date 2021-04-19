a8ffd1ece6738b7483be7663ce9d0d1353ac4b99
Kovács Dániel <h878969@stud.u-szeged.hu>
2021-04-19 10:20:38 +0200

    Modified as in Class no.2

--- app/src/main/java/com/example/bloodpressuremonitor/MainActivity.java
+++ app/src/main/java/com/example/bloodpressuremonitor/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.bloodpressuremonitor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    EditText usernameET;
+    EditText passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameET = findViewById(R.id.editTextUserName);
+        passwordET = findViewById(R.id.editTextPassword);
+    }
+
+    public void login(View view) {
+
+        String username = usernameET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        Log.i(LOG_TAG,"Bejelentkezett: " + username + ", jelszó: " + password);
     }
 }
